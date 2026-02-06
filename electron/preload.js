2031d5a5cdbab9f0d1752ff93f803872a97c0fd8
GEEDDIGA <115898952+GEEDDIGA@users.noreply.github.com>
2026-02-06 20:49:12 +0300

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -1,26 +1,21 @@
-const { contextBridge, ipcRenderer } = require('electron');
+// electron/preload.js
+const { contextBridge, ipcRenderer } = require("electron");
 
-// Expose protected methods that allow the renderer process to use
-// the ipcRenderer without exposing the entire object
-contextBridge.exposeInMainWorld('electron', {
-  // Transactions API
-  getTransactions: () => ipcRenderer.invoke('get-transactions'),
-  
-  // Stats API  
-  getStats: () => ipcRenderer.invoke('get-stats'),
-  
-  // Future APIs for Zaad automation
-  sendTransaction: (data) => ipcRenderer.invoke('send-transaction', data),
-  updateTransaction: (id, data) => ipcRenderer.invoke('update-transaction', id, data),
-  deleteTransaction: (id) => ipcRenderer.invoke('delete-transaction', id),
-  
-  // Listen for real-time updates from mobile app
+contextBridge.exposeInMainWorld("electron", {
+  getTransactions: () => ipcRenderer.invoke("get-transactions"),
+  getStats: () => ipcRenderer.invoke("get-stats"),
   onTransactionUpdate: (callback) => {
-    ipcRenderer.on('transaction-update', (event, data) => callback(data));
-  },
-  
-  // Remove listener
-  removeTransactionListener: (callback) => {
-    ipcRenderer.removeListener('transaction-update', callback);
+    if (typeof callback !== "function") return;
+
+    const listener = (_event, payload) => {
+      callback(payload);
+    };
+
+    ipcRenderer.on("transaction-update", listener);
+
+    // return unsubscribe
+    return () => {
+      ipcRenderer.removeListener("transaction-update", listener);
+    };
   }
 });
