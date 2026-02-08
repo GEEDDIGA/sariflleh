990f883b9e234c083466d65a763da251ef0b5283
GEEDDIGA <115898952+GEEDDIGA@users.noreply.github.com>
2026-02-08 17:00:00 +0300

    Fix App.tsx to use React Router properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 // src/App.tsx
-import IndexPage from "./pages/Index";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
 export default function App() {
-  return <IndexPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
